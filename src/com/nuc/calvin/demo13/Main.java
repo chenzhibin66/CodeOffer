b9f78ee18a7dd7f247978cc178fbc814585004fa
chen <m15903462872@163.com>
2019-05-26 16:38:08 +0800

    update code

---
+++ src/com/nuc/calvin/demo13/Main.java
@@ -0,0 +1,57 @@
+package com.nuc.calvin.demo13;
+
+import java.util.ArrayList;
+
+/**
+ * @author Calvin
+ * @Description:输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+ * 例如，如果输入如下4 X 4矩阵：
+ * 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+ * 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        System.out.println(printMatrix(matrix));
+    }
+
+    public static ArrayList<Integer> printMatrix(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        printMatrixHelp(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1, result);
+        return result;
+    }
+
+    private static void printMatrixHelp(int[][] matrix, int startRow, int startCol, int endRow, int endCol, ArrayList<Integer> result) {
+        if (startRow < endRow && startCol < endCol) {
+            for (int i = startCol; i <= endCol; i++) {
+                result.add(matrix[startRow][i]);
+            }
+            for (int i = startRow + 1; i <= endRow - 1; i++) {
+                result.add(matrix[i][endCol]);
+            }
+            for (int i = endCol; i >= startCol; i--) {
+                result.add(matrix[endRow][i]);
+            }
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                result.add(matrix[i][startCol]);
+            }
+            printMatrixHelp(matrix, startRow + 1, startCol + 1, endRow - 1, endCol - 1, result);
+        } else if (startRow == endRow && startCol < endCol) {
+            for (int i = startCol; i <= endCol; i++) {
+                result.add(matrix[endRow][i]);
+            }
+        } else if (startCol == endCol && startRow < endRow) {
+            for (int i = startRow; i <= endCol; i++) {
+                result.add(matrix[startCol][i]);
+            }
+        } else if (startRow == endRow && startCol == endCol) {
+            result.add(matrix[startRow][startCol]);
+        } else {
+            return;
+        }
+
+    }
+}
