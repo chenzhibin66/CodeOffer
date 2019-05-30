bd6c61280db0968e6d9df4c8a38b2a7a547fbc0a
chen <m15903462872@163.com>
2019-05-30 21:41:22 +0800

    update code

---
+++ src/com/nuc/calvin/demo16/Main.java
@@ -0,0 +1,17 @@
+package com.nuc.calvin.demo16;
+
+import java.util.ArrayList;
+
+/**
+ * @author Calvin
+ * @Description:从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+
+    }
+}
---
+++ src/com/nuc/calvin/demo16/TreeNode.java
@@ -0,0 +1,15 @@
+package com.nuc.calvin.demo16;
+
+/**
+ * @author Calvin
+ * @Description:
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
