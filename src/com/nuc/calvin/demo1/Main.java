2e9aa2aafd177ce1d0ae60ae4f9e7f7beaf141d1
chen <m15903462872@163.com>
2019-07-09 17:18:30 +0800

    update cpde

--- src/com/nuc/calvin/demo1/Main.java
+++ src/com/nuc/calvin/demo1/Main.java
@@ -2,8 +2,7 @@ package com.nuc.calvin.demo1;
 
 /**
  * @author Calvin
- * @Description:在一个二维数组中（每个一维数组的长度相同），
- * 每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * @Description:在一个二维数组中（每个一维数组的长度相同）， 每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * @Date:Created on 21:19 2019/5/13
  */
@@ -17,7 +16,7 @@ public class Main {
     public static boolean Find(int target, int[][] array) {
         int row = 0;
         int column = array.length - 1;
-        while (column > 0 && row <= array[0].length - 1) {
+        while (column >= 0 && row <= array[0].length - 1) {
             if (array[row][column] > target) {
                 column--;
             } else if (array[row][column] < target) {
