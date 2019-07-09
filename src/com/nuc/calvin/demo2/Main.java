d30dfa32ef5d44878d55be80beacd606cf02943d
chen <m15903462872@163.com>
2019-07-09 17:24:51 +0800

    update code

--- src/com/nuc/calvin/demo2/Main.java
+++ src/com/nuc/calvin/demo2/Main.java
@@ -8,23 +8,24 @@ package com.nuc.calvin.demo2;
 public class Main {
     public static void main(String[] args) {
         StringBuffer str = new StringBuffer("We Are Happy");
-        System.out.println(replaceSpace(str));;
+        System.out.println(replaceSpace(str));
+        ;
     }
 
     public static String replaceSpace(StringBuffer str) {
         if (str == null) {
             return null;
         }
-        StringBuffer newStr = new StringBuffer();
+        StringBuffer newS = new StringBuffer();
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ' ') {
-                newStr.append('%');
-                newStr.append('2');
-                newStr.append('0');
+                newS.append('%');
+                newS.append('2');
+                newS.append('0');
             } else {
-                newStr.append(str.charAt(i));
+                newS.append(str.charAt(i));
             }
         }
-        return newStr.toString();
+        return newS.toString();
     }
 }
